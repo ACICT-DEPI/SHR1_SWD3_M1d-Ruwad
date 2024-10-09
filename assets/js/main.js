5377fc4d00728174396d8ad2e4e40d46fe71e313
Youssef <youssefyasser752@gmail.com>
2024-10-10 02:31:27 +0300

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
 
 /*=============== SHOW MENU ===============*/
 
+// const navMenu=document.getElementById("nav-menu"),
+//         navToggle=document.getElementById("nav-toggle"),
+//         navClose=document.getElementById("nav-close")
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
+// if(navToggle){
+//     navToggle.addEventListener("click",()=>{
+//         navMenu.classList.add("show-menu")
+//     })
+// }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
+// if(navClose){
+//     navClose.addEventListener("click",()=>{
+//         navMenu.classList.remove("show-menu")
+//     })
+// }
 
 /*=============== SHOW CART ===============*/
 const cart=document.getElementById("cart"),
@@ -50,12 +63,30 @@ if(loginButton){
 
 
 /*=============== HOME SWIPER ===============*/
-
+// var homeSwiper = new Swiper(".home-swiper", {
+//     spaceBetween: 30,
+//     loop: true,
+    
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
+// function scrollHeader() {
+//     const header = document.getElementById("header");
+//     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+//     if(this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+// }
+// window.addEventListener("scroll", scrollHeader);
 /*=============== NEW SWIPER ===============*/
+// var newSwiper = new Swiper(".new-swiper", {
+//     spaceBetween: 16,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     loop: true,
+// });
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
