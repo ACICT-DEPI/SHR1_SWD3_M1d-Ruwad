7f0d53f744d3927e47f0c21c9c649bb808450e5d
Your ibrahem <ibrahem.elahmedy@gmail.com>
2024-10-12 16:04:19 +0300

    handle yousif work and with js menu responsive button & background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
 
 /*=============== SHOW MENU ===============*/
 
-// const navMenu=document.getElementById("nav-menu"),
-//         navToggle=document.getElementById("nav-toggle"),
-//         navClose=document.getElementById("nav-close")
+const navMenu=document.getElementById("nav-menu"),
+        navToggle=document.getElementById("nav-toggle"),
+        navClose=document.getElementById("nav-close")
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
-// if(navToggle){
-//     navToggle.addEventListener("click",()=>{
-//         navMenu.classList.add("show-menu")
-//     })
-// }
+if(navToggle){
+    navToggle.addEventListener("click",()=>{
+        navMenu.classList.add("show-menu")
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-// if(navClose){
-//     navClose.addEventListener("click",()=>{
-//         navMenu.classList.remove("show-menu")
-//     })
-// }
+if(navClose){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove("show-menu")
+    })
+}
 
 /*=============== SHOW CART ===============*/
 const cart=document.getElementById("cart"),
@@ -74,12 +74,12 @@ if(loginButton){
 // });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-// function scrollHeader() {
-//     const header = document.getElementById("header");
-     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-//     if(this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
-// }
-// window.addEventListener("scroll", scrollHeader);
+function scrollHeader() {
+    const header = document.getElementById("header");
+    //  When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 /*=============== NEW SWIPER ===============*/
 // var newSwiper = new Swiper(".new-swiper", {
 //     spaceBetween: 16,
