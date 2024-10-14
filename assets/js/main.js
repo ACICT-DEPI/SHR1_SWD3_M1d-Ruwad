43fce7f90da6bc68bec85eb40114befa19254800
yasmena <129227497+Yasmeen-Mahmoud@users.noreply.github.com>
2024-10-15 00:33:11 +0300

    Details page (add lightbox script )

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,68 +103,6 @@ window.addEventListener("scroll",scrollUp)
 
 
 /*=============== LIGHT BOX ===============*/
-const productItems = document.querySelectorAll(".product__img"),
-    totalProductItems = productItems.length,
-    lightbox = document.querySelector(".lightbox"),
-    lightboxImg = lightbox.querySelector(".lightbox__img"),
-    lightboxClose = lightbox.querySelector(".lightbox__close"),
-    lightboxCounter = lightbox.querySelector(".caption__counter");
-
-let itemIndex = 0;
-
-for (let i=0; i<totalProductItems; i++)
-{
-    productItems[i].addEventListener("click", function() {
-        itemIndex = i;
-        changeItem();
-        toggleLightbox();
-    });
-}
-
-function nextItem() {
-    if (itemIndex === totalProductItems-1) 
-    {
-        itemIndex = 0;
-    }
-    else
-    {
-        itemIndex++;
-    }
-
-    changeItem();
-}
-
-function prevItem() {
-    if (itemIndex === 0) 
-    {
-        itemIndex = totalProductItems-1;
-    }
-    else
-    {
-        itemIndex--;
-    }
-
-    changeItem();
-}
-
-function toggleLightbox() {
-    lightbox.classList.toggle("open");
-};
-
-function changeItem() {
-    imgSrc = productItems[itemIndex].querySelector(".product__img img").getAttribute("src");
-    lightboxImg.src = imgSrc;
-    lightboxCounter.innerHTML = (itemIndex + 1) + " of " + totalProductItems;
-};
-
-// close lightbox
-lightbox.addEventListener("click", function() {
-    if (event.target === lightboxClose || event.target === lightbox) 
-    {
-        toggleLightbox();
-    }
-});
-
 
 /*=============== QUESTIONS ACCORDION ===============*/
 
