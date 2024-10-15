04f234863f09f00ffcc3ea1e432ff73cec1ee174
Your ibrahem <ibrahem.elahmedy@gmail.com>
2024-10-15 13:27:45 +0300

    switch color theme with some edite

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,26 +256,21 @@ document.getElementById('register-form')?.addEventListener('submit', function (e
         phone: regPhone,
         address: regAddress
     };
-console.log(newUser);
 
     // Save the new user in the users array and localStorage
     users.push(newUser);
     localStorage.setItem('users', JSON.stringify(users));
-    
-    window.location.href = 'index.html'; // Redirect to login
-    login.classList.add("show-login")
+    // document.getElementById("login__redirect").addEventListener("click",()=>{
+        
+    //     window.location.href = 'index.html'; // Redirect to login
+    //     setInterval(()=>{
+
+    //         login.classList.add("show-login")
+    //     },3000)
+    // })
 });
 
 
-/*=============== login handle ===============*/
-// Login logic
-let isAuthanticated;
-const loginFrom=document.getElementById('login-form')
-loginFrom.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const errorMessageDiv = document.getElementById('error-message');
 
 
 /*=============== dynamic products  ===============*/
@@ -287,10 +282,12 @@ let products=[];
 let label = document.getElementById("cart__prices-total")
 let cartContainer=document.getElementById("cart__container")
 let totalCartItems=document.getElementById("cart__prices-item")
-    // ###################################################################increament number
 
+
+// ################################################################### create shop page producr
 window.addEventListener('DOMContentLoaded', () => {
-fetch('/assets/json/products.json')
+fetch('assets/json/products.json')
+// fetch('/assets/json/products.json')
         .then(response => response.json())
         .then(data => {
             return data.forEach(product => {
@@ -319,7 +316,7 @@ fetch('/assets/json/products.json')
         });
          // Add to cart function
     window.addToCart = function (productId) {
-        fetch('/assets/json/products.json')
+        fetch('assets/json/products.json')
             .then(response => response.json())
             .then(data => {
             products.push(data)
@@ -335,8 +332,10 @@ fetch('/assets/json/products.json')
     
     totalAmount()
 })
+// ###################################################################displayCart to create card section item
+
 function displayCart() {
-    fetch('/assets/json/products.json')
+    fetch('assets/json/products.json')
         .then(response => response.json())
         .then(data => {
             if (basket.length !== 0) {
@@ -387,132 +386,8 @@ function displayCart() {
     }
 
 
-
-let increament = (id) => {
-    let selectedItem = id
-    let productIsSelectedAgain = basket.find((cart) => cart.id == selectedItem)
-    if (!productIsSelectedAgain ) {
-        basket.push(
-            {
-                id: selectedItem,
-                item: 1,
-            }
-        )
-        
-    } else {
-        productIsSelectedAgain.item+=1; 
-    }
-
-});
-/*=============== dynamic products  ===============*/
-let shopItems=document.getElementById("shop__items");
-let cartPricesInSection=document.getElementById("cart__prices")
-let emptyCard=document.getElementById("empty-card")
-let basket=JSON.parse(localStorage.getItem('productSelected')) ||[]
-let products=[];
-let label = document.getElementById("cart__prices-total")
-let cartContainer=document.getElementById("cart__container")
-let totalCartItems=document.getElementById("cart__prices-item")
-    // ###################################################################increament number
-
-window.addEventListener('DOMContentLoaded', () => {
-fetch('/assets/json/products.json')
-        .then(response => response.json())
-        .then(data => {
-            return data.forEach(product => {
-                products.push(product)
-                const productDiv = document.createElement('div');
-                productDiv.classList.add('product');
-                productDiv.innerHTML =`<div class="shop__content" id="${product.id}">
-                                            <a href="details.html"  '>
-                                                <div class="shop__tag" >${product.category}</div>
-                                                <img src="${product.images[0]}" alt="${product.name}" class="shop__img">
-                                                <h3 class="shop__title">${product.name}</h3>
-                                                <span class="shop__subtitle">Accessory </span>
-                                                <div class="shop__prices">
-                                                    <span class="shop__price">$${product.price}</span>
-                                                    <span class="shop__discounts">$${product.stock}</span>
-                                                </div>
-                                            </a>
-                                            <i class="bx bx-cart-alt shop__icon add-to-card button shop__button" onclick="addToCart(${product.id})"></i>
-                                        </div>`;
-                                        if(shopItems){
-
-                                            shopItems.appendChild(productDiv);
-                                        }
-                return products
-            });
-        });
-         // Add to cart function
-    window.addToCart = function (productId) {
-        fetch('/assets/json/products.json')
-            .then(response => response.json())
-            .then(data => {
-            products.push(data)
-                // const product = data.find(p => p.id === productId);
-                //     products.push(product)
-                // carts=carts.filter(cart=>cart.id!=product.id)
-                increament(productId)
-                displayCart();
-            });
-    };
-    // Display cart items
-    displayCart()
     
-    totalAmount()
-})
-function displayCart() {
-    fetch('/assets/json/products.json')
-        .then(response => response.json())
-        .then(data => {
-            if (basket.length !== 0) {
-        emptyCard.innerHTML=``;
-         (cartContainer.innerHTML = basket.map((item) => {
-            let productSelected;
-            
-            productSelected = data.find((x) => x.id === item.id)||[]
-            if(productSelected){
-            // for empty card section
-           
-        return  `   <article class="cart__card">
-                        <div class="cart__box">
-                            <img src="${productSelected.images[0]}" alt="${productSelected.name}" class="cart__img">
-                        </div>
-                        <div class="cart__details">
-                                <h3 class="cart__title">
-                                ${productSelected.name}
-                                </h3>
-                                <span class="cart__price">$${productSelected.price * item.item}</span>
-                            <div class="cart__amount">
-                                <div class="cart__amount-content">
-                                    <span class="cart__amount-box">
-                                        <i class="bx bx-minus" onclick="decrement(${productSelected.id})"></i>
-                                    </span>
-                                    <span class="cart__amount-number" data-id=${productSelected.id}>${productSelected ? item.item : "1"}
-                                    </span>
-                                    <span class="cart__amount-box">
-                                        <i class="bx bx-plus"  onclick="increament(${productSelected.id})"></i>
-                                    </span>
-                                </div>
-                                <i class="bx bx-trash-alt cart__amount-trash" onclick="removeItem(${productSelected.id})"></i>
-                            </div>
-                        </div>
-                    </article>`;
-                }
-            }).join(" "))}else{
-                //################################# for empty card section
-                    emptyCard.innerHTML =`
-                    <h2 class='cart-empty'>
-                    Cart is empty </h2>
-                    `;
-                    
-                    return cartContainer.innerHTML=``
-                }
-        })
-           
-    }
-
-
+// ###################################################################increament number
 
 let increament = (id) => {
     let selectedItem = id
@@ -528,7 +403,6 @@ let increament = (id) => {
     } else {
         productIsSelectedAgain.item+=1; 
     }
-
     update(selectedItem);
     localStorage.setItem("productSelected", JSON.stringify(basket))
     displayCart()
@@ -549,36 +423,41 @@ let increament = (id) => {
     displayCart()
 }
 // ###################################################################EVERY update
+
 function update (id)  {
-    let selectditems = basket.find((cart) => cart.id == id)
-    if (selectditems.item < 0) {
-        return
-    }
-    else {
-   if(document.querySelector(`[data-id="${id}"]`)){
-    
-       document.querySelector(`[data-id="${id}"]`).innerHTML=selectditems.item
-       if(selectditems.item<=0){
-        
-       }
-   }
-    }
-    totalAmount()
-    totalSumProduct()
-}
-let totalSumProduct = () => {
-    let basketSum = document.querySelector(".cart-counter")
 
-    if(basketSum){
+                            let selectditems = basket.find((cart) => cart.id == id)
+                            if (selectditems.item < 0) {
+                                return
+                            }
+                            else {
+                        if(document.querySelector(`[data-id="${id}"]`)){
+                            
+                            document.querySelector(`[data-id="${id}"]`).innerHTML=selectditems.item
+                            if(selectditems.item<=0){
+                                
+                            }
+                        }
+                            }
+                            totalAmount()
+                            totalSumProduct()
+                        }
+// ###################################################################totalSumProduct
 
-        return basketSum.innerHTML = basket.reduce((x, y) => x + y.item, 0)
+let totalSumProduct = () => 
+    {
+        let basketSum = document.querySelector(".cart-counter")
+        if(basketSum){
+
+            return basketSum.innerHTML = basket.reduce((x, y) => x + y.item, 0)
+        }
     }
-
-}
 totalSumProduct()
+
+// ###################################################################totalAmount
 let totalAmount = () => {
     if (basket.length !== 0) {
-        fetch('/assets/json/products.json')
+        fetch('assets/json/products.json')
         .then(response => response.json())
         .then(data => {
         let ammount = basket.map((x) => {
@@ -598,12 +477,16 @@ let totalAmount = () => {
     }
 }
 totalAmount()
+// ###################################################################removeAll
+
 let removeAll = () => {
     basket = []
     createCartItem();
     totalSumProduct()
     localStorage.setItem("productSelected", JSON.stringify(carts))
 }
+// ###################################################################removeItem
+
 let removeItem = (id) => {
     let itemselected = id
     basket = basket.filter((x) => x.id !== itemselected
@@ -614,4 +497,3 @@ let removeItem = (id) => {
     localStorage.setItem("productSelected", JSON.stringify(basket))
 
 }
-
